359658f4494374c00f54972429c218e4dd588a54
Tom Rhodes <thomas.rhodes@simulity.com>
2015-01-17 17:12:04 +0000

    change header

--- src/calc/Gui.java
+++ src/calc/Gui.java
@@ -1,28 +1,5 @@
-/*
- * 
- * 
- * Simulity Labs Ltd.
- * 
- * Copyright (C) Simulity Labs Ltd. All rights reserved.
- * 
- * This source code is the property of Simulity Labs Ltd. Redistribution and
- * use in source (source code) or binary (object code) forms with or without 
- * modification, for commercial, educational or research purposes is not
- * permitted without the prior written consent of Simulity Labs Limited 
- * 
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE, UNLESS PRIOR WRITTEN CONSENT STATES OTHERWISE.
- * 
- */
+//TDIZZLE INNIT.
+
 package calc;
 
 import java.util.logging.Level;
